2d2ba386b7d905bb7b9effa446efb9fa918e6f89
Nichole Axel <nfrancis22@gmail.com>
2022-05-01 21:06:23 -0400

    adding to collection

--- index.js
+++ index.js
@@ -6,4 +6,35 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const db = admin.firestore();
 
+// const clinic = {
+//     name: 'JBAH',
+//     location: '12834 US Hwy 1',
+//     phone: '561-626-8000',
+//     type: 'Small Animal',
+//     exotic: false,
+//     corporate: true,
+// }
+
+// db.collection('clinics').add(clinic)
+
+const clinicCol = db.collection('clinics')
+
+const clinic2 = {
+    name: 'Bluffs',
+    location: '13440 US HWY 1',
+    phone: '561-626-2318',
+    type: 'Small Animal',
+    exotic: false,
+    coporate: false,
+}
+clinicCol.add(clinic2)
+
+// clinicCol.doc(clinic2).set ({
+//     leadDvm: 'Dr. Bob Horwitz',
+//     associateDvms: 2,
+// }) this did not work
+
+/*since i am using the short hand, how to I add to the document
+clinic 2? How do i fix 'coporate' to corporate? */
